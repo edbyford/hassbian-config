0fd9ffb7fb576d22f2092e0cdb6fe840020f42ea
Ed <edbyford@gmail.com>
2017-11-04 18:32:02 +0000

    delete extra script

--- changelightcolour.py
+++
@@ -1,5 +0,0 @@
-import appdaemon.appapi as appapi
-
-class ChangeLightColour(appapi.AppDaemon):
-
-self.handle = self.listen_state(self.before_sunrise_cb, "light", new = "on", duration = 30, immediate = True, colour)
