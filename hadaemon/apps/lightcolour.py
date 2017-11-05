3d27d3171a1990c53af5d21e15e4b2390318bb77
Ed <edbyford@gmail.com>
2017-11-05 17:56:46 +0000

    remote turn off

--- hadaemon/apps/lightcolour.py
+++ hadaemon/apps/lightcolour.py
@@ -7,6 +7,6 @@ class LightColour(appapi.AppDaemon):
 
     def lights_on(self, entity, attribute, old, new, kwargs):
         if self.now_is_between("sunset - 00:30:00", "sunrise - 00:30:00"):
-            self.turn_on(self.args["activelight"], brightness = 180, color_name = "sandy brown")
+            self.turn_on(self.args["activelight"], brightness = 200, color_name = "sandy brown" transition = 30)
         else:
             self.turn_on(self.args["activelight"], brightness = 240, rgb_color = [255,222,173])
